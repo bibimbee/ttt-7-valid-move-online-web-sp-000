85e8a3a07792e50da82a30f10b0b2b4d6b5c8be9
Emma Danica <ms.bee.indeed@gmail.com>
2020-10-13 01:24:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,15 +7,15 @@ def position_taken?(board, index)
   end
 end
 
-def on_board?(num)
-  if num.between?(0,8) == true
+def on_board?(index)
+  if index.between?(0,8) == true
     return true
   else
     return false
   end
 end
 
-if (position_taken?(board,index)) == false && (on_board?(num)) == true
+if (position_taken?(board,index)) == false && (on_board?(index)) == true
   return true
 else
   return false
